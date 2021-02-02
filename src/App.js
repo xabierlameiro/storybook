42b9c2bd7645ce70c5e3c8d29113ca613d44584f
xlameiro <xabier.lameiro@gmail.com>
2021-02-02 22:40:27 +0100

    Testing automatic deploy on firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola Mundo</h1>
+        <h1>Hola Mundo test </h1>
       </header>
     </div>
   );
